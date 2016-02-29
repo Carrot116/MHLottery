74a51019ef034a69173cf0c4366b10a63068e540
Carrot116 <huxueli1986@163.com>
2016-03-01 00:45:10 +0800

    first commit

---
+++ MHLottery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MHLottery
+//
+//  Created by carrot on 16/2/29.
+//  Copyright © 2016年 carrot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
